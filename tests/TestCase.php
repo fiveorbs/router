61596e98d50a7de0064a54275d2652f354696eb0
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2023-11-01 10:56:16 +0100

    Add TestCase::fullTrim

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,10 +5,15 @@ declare(strict_types=1);
 namespace Conia\Route\Tests;
 
 use Conia\Registry\Registry;
+use Conia\Route\Renderer\HtmlRenderer;
+use Conia\Route\Renderer\JsonRenderer;
+use Conia\Route\Renderer\Renderer;
+use Conia\Route\Renderer\TextRenderer;
 use Laminas\Diactoros\ResponseFactory;
 use Laminas\Diactoros\ServerRequestFactory;
 use Laminas\Diactoros\Uri;
 use PHPUnit\Framework\TestCase as BaseTestCase;
+use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 class TestCase extends BaseTestCase
@@ -43,6 +48,12 @@ class TestCase extends BaseTestCase
         bool $autowire = true,
     ): Registry {
         $registry = new Registry(autowire: $autowire);
+        $registry->add(ResponseFactoryInterface::class, $this->responseFactory());
+
+        $rendererTag = $registry->tag(Renderer::class);
+        $rendererTag->add('text', TextRenderer::class);
+        $rendererTag->add('json', JsonRenderer::class);
+        $rendererTag->add('html', HtmlRenderer::class);
 
         return $registry;
     }
@@ -50,5 +61,20 @@ class TestCase extends BaseTestCase
     public function responseFactory(): ResponseFactory
     {
         return new ResponseFactory();
+    }
+
+    public function fullTrim(string $text): string
+    {
+        return trim(
+            preg_replace(
+                '/> </',
+                '><',
+                preg_replace(
+                    '/\s+/',
+                    ' ',
+                    preg_replace('/\n/', '', $text)
+                )
+            )
+        );
     }
 }
