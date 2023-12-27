5868b16b5f5ba3f7ea28fcf5dc765ddc16dc4b56
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2023-12-27 17:29:27 +0100

    Make Render attribute a simple information container

--- src/Renderer/Render.php
+++ src/Renderer/Render.php
@@ -5,9 +5,6 @@ declare(strict_types=1);
 namespace Conia\Route\Renderer;
 
 use Attribute;
-use Conia\Registry\Registry;
-use Conia\Route\Renderer\Renderer;
-use Psr\Http\Message\ResponseInterface as Response;
 
 /** @psalm-api */
 #[Attribute]
@@ -18,23 +15,5 @@ class Render
     public function __construct(protected string $renderer, mixed ...$args)
     {
         $this->args = $args;
-    }
-
-    public function render(Registry $registry, mixed $data): string
-    {
-        return $this->getRenderer($registry)->render($data, ...$this->args);
-    }
-
-    public function response(Registry $registry, mixed $data): Response
-    {
-        return $this->getRenderer($registry)->response($data, ...$this->args);
-    }
-
-    protected function getRenderer(Registry $registry): Renderer
-    {
-        $renderer = $registry->tag(Renderer::class)->get($this->renderer);
-        assert($renderer instanceof Renderer);
-
-        return $renderer;
     }
 }
