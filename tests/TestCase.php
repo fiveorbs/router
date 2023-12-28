4898dbb63c2d19f2e3bc15c5afbbc044d2e26a7e
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2023-12-28 19:54:01 +0100

    Remove TestCase::registry

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,20 +4,21 @@ declare(strict_types=1);
 
 namespace Conia\Route\Tests;
 
-use Conia\Registry\Registry;
-use Conia\Route\Renderer\HtmlRenderer;
-use Conia\Route\Renderer\JsonRenderer;
-use Conia\Route\Renderer\Renderer;
-use Conia\Route\Renderer\TextRenderer;
 use Laminas\Diactoros\ResponseFactory;
 use Laminas\Diactoros\ServerRequestFactory;
 use Laminas\Diactoros\Uri;
 use PHPUnit\Framework\TestCase as BaseTestCase;
-use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 class TestCase extends BaseTestCase
 {
+    public string $root;
+
+    public function setUp(): void
+    {
+        $this->root = __DIR__ . '/Fixtures';
+    }
+
     public function throws(string $exception, string $message = null): void
     {
         $this->expectException($exception);
@@ -42,20 +43,6 @@ class TestCase extends BaseTestCase
         }
 
         return $request;
-    }
-
-    public function registry(
-        bool $autowire = true,
-    ): Registry {
-        $registry = new Registry(autowire: $autowire);
-        $registry->add(ResponseFactoryInterface::class, $this->responseFactory());
-
-        $rendererTag = $registry->tag(Renderer::class);
-        $rendererTag->add('text', TextRenderer::class);
-        $rendererTag->add('json', JsonRenderer::class);
-        $rendererTag->add('html', HtmlRenderer::class);
-
-        return $registry;
     }
 
     public function responseFactory(): ResponseFactory
